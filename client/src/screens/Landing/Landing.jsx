c459f7598b62c4afbef4d8cc1e1500c5334ac0a0
gradyki <grady.king07@gmail.com>
2021-08-20 17:34:41 -0400

    landing page search added and route to searched page

--- client/src/screens/Landing/Landing.jsx
+++ client/src/screens/Landing/Landing.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import { useHistory } from "react-router";
+import { useState } from "react";
 
 export default function Landing(props) {
-  return <div>Landing page</div>;
+  const [search, setSearch] = useState(null);
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    history.push(`/zipcode/${search}`);
+  };
+
+  return (
+    <div>
+      <div>Landing page</div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="search"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <button>Submit</button>
+        </form>
+      </div>
+    </div>
+  );
 }
