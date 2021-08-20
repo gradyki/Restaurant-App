563671835e1cd309932539ce4d07f5a2a7e5d8b6
gradyki <grady.king07@gmail.com>
2021-08-20 17:27:30 -0400

    zipcode rest FE route setup

--- client/src/containers/MainContainer.jsx
+++ client/src/containers/MainContainer.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import { useEffect, useHistory } from "react";
 import { Switch, Route } from "react-router-dom";
 import Landing from "../screens/Landing/Landing";
+import ZipCodeRests from "../screens/ZipCodeRests/ZipCodeRests";
 
 export default function MainContainer(props) {
   return (
     <Switch>
+      <Route path="/zipcode/:id">
+        <ZipCodeRests />
+      </Route>
       <Route exact path="/">
         <Landing />
       </Route>
