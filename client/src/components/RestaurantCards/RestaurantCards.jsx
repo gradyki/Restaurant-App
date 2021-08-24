8e14f5ab3fe5c7ef0d152a065b64ea3c03e18b8e
gradyki <grady.king07@gmail.com>
2021-08-24 17:49:07 -0400

    all restaurant info rendering for cards for search

--- client/src/components/RestaurantCards/RestaurantCards.jsx
+++ client/src/components/RestaurantCards/RestaurantCards.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./RestaurantCards.css";
 
 export default function RestaurantCards(props) {
   const { restaurants } = props;
@@ -6,8 +7,18 @@ export default function RestaurantCards(props) {
   return (
     <div className="cards-container">
       {restaurants.map((restaurant) => (
-        <div key={restaurant.id}>
-          <h1>{restaurant.restaurant_name}</h1>
+        <div className="cards">
+          <div className="googlemap-container">
+            <img src="" alt="geolocation placeholder" />
+          </div>
+          <div key={restaurant.id} className="cards-info-container">
+            <a href={restaurant.restaurant_website} target="_blank">
+              <h1>{restaurant.restaurant_name}</h1>
+              <h3>{restaurant.address.formatted}</h3>
+              <h3>{restaurant.restaurant_phone}</h3>
+              <h3>{restaurant.cuisines[0]}</h3>
+            </a>
+          </div>
         </div>
       ))}
     </div>
