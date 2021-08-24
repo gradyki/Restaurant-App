20e205dbf3fe73295b56b88d9698f476a9f25aea
gradyki <grady.king07@gmail.com>
2021-08-24 14:02:17 -0400

    concat working for zip codes with more than 100 results for api call

--- client/src/screens/ZipCodeRests/ZipCodeRests.jsx
+++ client/src/screens/ZipCodeRests/ZipCodeRests.jsx
@@ -10,8 +10,6 @@ export default function ZipCodeRests() {
   const [isLoaded, setIsLoaded] = useState(false);
   const searched = useParams();
 
-  let restData = [];
-
   console.log("zipcode parsed", searched.id);
 
   useEffect(() => {
@@ -20,36 +18,30 @@ export default function ZipCodeRests() {
       let response = await axios.get(
         `https://api.documenu.com/v2/restaurants/zip_code/${zip}?size=100&page=1&key=${key}`
       );
-      setRestaurants(response.data);
-      setIsLoaded(true);
+      let restData = await response.data;
+      if (restData.total_pages === 1) {
+        setRestaurants(restData.data);
+        setIsLoaded(true);
+      } else {
+        for (let i = 2; i <= restData.total_pages; i++) {
+          let multipleResponses = restData.data;
+          let multipleCalls = await axios.get(
+            `https://api.documenu.com/v2/restaurants/zip_code/${zip}?size=100&page=${i}&key=${key}`
+          );
+          let toConcat = await multipleCalls.data.data;
+          multipleResponses.push(toConcat);
+          console.log("multiple responses array", multipleResponses);
+          multipleResponses = multipleResponses.reduce(
+            (a, b) => a.concat(b),
+            []
+          );
+          setRestaurants(multipleResponses);
+          setIsLoaded(true);
+        }
+      }
     };
     getRestaurants();
   }, [searched]);
-
-  // useEffect(() => {
-  //   const getRestaurants = async () => {
-  //     let response = await axios.get(
-  //       `https://api.documenu.com/v2/restaurants/zip_code/${parseInt(
-  //         searched.id
-  //       )}?size=100&page=1&key=cc6decb23a852d654db440c3f36a5e2d`
-  //     );
-  //     restData.push(response.data);
-  //     if (response.more_pages === true) {
-  //       for (let i = 2; i <= response.total_pages; i++) {
-  //         response = await axios.get(
-  //           `https://api.documenu.com/v2/restaurants/zip_code/${parseInt(
-  //             searched.id
-  //           )}?size=100&page=${i}&key=cc6decb23a852d654db440c3f36a5e2d`
-  //         );
-  //         restData.push(response.data);
-  //       }
-  //       restData = restData.reduce((a, b) => a.concat(b), []);
-  //       setRestaurants(restData);
-  //     }
-  //     setIsLoaded(true);
-  //   };
-  //   getRestaurants();
-  // }, [searched]);
 
   if (!isLoaded) {
     return <div>Loading...</div>;
@@ -58,31 +50,10 @@ export default function ZipCodeRests() {
     return (
       <div>
         <div>Zip Code Searched Page</div>
-        <div>{restaurants.data[0].restaurant_name}</div>
         <div>
-          <RestaurantCards restaurants={restaurants.data} />
+          <RestaurantCards restaurants={restaurants} />
         </div>
       </div>
     );
   }
 }
-
-// let restData = []
-// const getRestaurants = async () => {
-//   let response = await fetch(`https://api.documenu.com/v2/restaurants/zip_code/10001?size=100&page=1&key=cc6decb23a852d654db440c3f36a5e2d`)
-//   let dat = await response.json()
-//   restData.push(dat.data)
-//   if (dat.more_pages === true) {
-//     for (let i = 2; i <= dat.total_pages; i++){
-//       response = await fetch(`https://api.documenu.com/v2/restaurants/zip_code/10001?size=100&page=${i}&key=cc6decb23a852d654db440c3f36a5e2d`)
-//       dat = await response.json()
-//       restData.push(dat.data)
-//     }
-//     restData = restData.reduce((a, b) => a.concat(b), []);
-//     console.log('rest data after loop', restData)
-//   }
-// }
-
-// console.log('outside', restData)
-
-// getRestaurants()
